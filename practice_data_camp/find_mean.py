d2c76bc6add9548332a5086c2c1fa71ad775f851
Tolani99 <tolaniadisa999@gmail.com>
2023-03-18 18:03:18 +0100

    Sorting and subsetting dataframes

---
+++ practice_data_camp/find_mean.py
@@ -0,0 +1,12 @@
+"""
+To find the mean of some numbers
+"""
+# Import numpy as np
+import numpy as np
+
+# Use np on the arrays of numbers
+np_array = np.array([323, 234, 342, 392, 212])
+
+# Compute the mean
+print(np.mean(np_array))
+
