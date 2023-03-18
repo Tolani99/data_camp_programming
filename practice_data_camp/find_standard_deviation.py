96ebb38125826f3cd4a37e29c2c4b0ccbb0ad92e
Tolani99 <tolaniiiadisa@gmail.com>
2023-03-18 08:15:46 +0100

    To find the standard deviation of some numbers

---
+++ practice_data_camp/find_standard_deviation.py
@@ -0,0 +1,12 @@
+"""
+To find the standard deviation of 
+some set of numbers
+"""
+# Import numpy as np
+import numpy as np
+
+# Use the np on the array
+x = np.array([3.6, 3.99, 3.91])
+
+# Compute the standard deviation
+print(np.std(x))
